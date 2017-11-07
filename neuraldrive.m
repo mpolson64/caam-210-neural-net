9e80700b42640252cec54996de4e0bbc88c87138
Miles Olson <mpolson64@gmail.com>
2017-11-06 22:01:03 -0600

    Read the test data

---
+++ neuraldrive.m
@@ -0,0 +1,9 @@
+function neuraldrive
+%Read training data and labels
+trainingdata = csvread('mnist_train.csv');
+digits = zeros(28, 28, 100);
+for i = 1:5999
+    digits(:, :, i) = reshape(trainingdata(i, 2:end), [28, 28])';
+end
+labels = trainingdata(:, 1);
+end
