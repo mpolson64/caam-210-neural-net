3c82354afd90b2f6b0cc2df11dae6797499af5ff
Miles Olson <mpolson64@gmail.com>
2017-11-08 11:28:27 -0600

    Corrected digits prealocation

--- neuraldrive.m
+++ neuraldrive.m
@@ -1,7 +1,7 @@
 function neuraldrive
 %Read training data and labels
 trainingdata = csvread('mnist_train.csv');
-digits = zeros(28, 28, 100);
+digits = zeros(28, 28, 5999);
 for i = 1:5999
     digits(:, :, i) = reshape(trainingdata(i, 2:end), [28, 28])';
 end
