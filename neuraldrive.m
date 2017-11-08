066547fa5c78c9edabed39743606edd72c48dbbb
Miles Olson <mpolson64@gmail.com>
2017-11-08 11:45:01 -0600

    Added an expected value vector to be mse'd against the network's output

--- neuraldrive.m
+++ neuraldrive.m
@@ -1,9 +1,14 @@
 function neuraldrive
 %Read training data and labels
 trainingdata = csvread('mnist_train.csv');
-digits = zeros(28, 28, 5999);
-for i = 1:5999
+digits = zeros(28, 28, 6000);
+for i = 1:6000
     digits(:, :, i) = reshape(trainingdata(i, 2:end), [28, 28])';
 end
+
 labels = trainingdata(:, 1);
+expectedvalues = zeros(10, 1, 6000);
+for i = 1:6000
+   expectedvalues(labels(i) + 1, 1, i) = 1;
+end
 end
