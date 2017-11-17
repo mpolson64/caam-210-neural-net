ed3181aac108ee31852d2ef23790b0a376c3e593
thesalmonification <33437091+thesalmonification@users.noreply.github.com>
2017-11-17 09:10:36 -0600

    Update score.m

--- score.m
+++ score.m
@@ -8,7 +8,7 @@
 function val = score(W1, W2, W3, Xs, labels, samplesize)
 correct = 0;
 for i = 1:samplesize
-    roll = floor(rand() * samplesize) + 1;
+    roll = floor(rand() * samplesize) + 1; %chooses a random number for the Network to evaluate on
     guess = evaluate(Xs(:, :, roll), W1, W2, W3);
     
     if guess == labels(roll)
