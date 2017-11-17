f9b87709420c0a1befc346ed19e14d36c26adfb9
thesalmonification <33437091+thesalmonification@users.noreply.github.com>
2017-11-16 19:34:43 -0600

    Update cost.m

--- cost.m
+++ cost.m
@@ -3,7 +3,7 @@
 %Inputs: y_ is the calculated output layer matrix using the neural network. y is the
 %     actual valued matrix the output layer should look like.
 %Output: J is the cost of the expected and calculated values of the output layer matrix.
-%     For purposes making the calculus of the Neural Network easier, the cost function
+%     For purposes of making the calculus of the Neural Network easier, the cost function
 %     performs half squared error, instead of more traditional mean squared error. 
 function J = cost(y_, y)
 J = 0.5 * sum((y - y_) .^ 2);
