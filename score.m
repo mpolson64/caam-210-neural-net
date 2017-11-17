6133034fbf2bf526dac59c67da34045dd892cd4d
Miles Olson <mpolson64@gmail.com>
2017-11-17 11:55:10 -0600

    oops

--- score.m
+++ score.m
@@ -8,11 +8,8 @@
 function val = score(W1, W2, W3, Xs, labels, samplesize)
 correct = 0;
 for i = 1:samplesize
-<<<<<<< HEAD
     roll = floor(rand() * size(Xs, 3)) + 1; %chooses a random number for the Network to evaluate on
-=======
-    roll = floor(rand() * samplesize) + 1; %chooses a random number for the Network to evaluate on
->>>>>>> thesalmonification-patch-1
+
     guess = evaluate(Xs(:, :, roll), W1, W2, W3);
     
     if guess == labels(roll)
