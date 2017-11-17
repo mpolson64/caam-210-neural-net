4b0dd94feffea46ca4c6d4bf88c30c0f50d8ec82
thesalmonification <33437091+thesalmonification@users.noreply.github.com>
2017-11-16 18:51:00 -0600

    Update evaluate.m

--- evaluate.m
+++ evaluate.m
@@ -1,3 +1,13 @@
+%function evaluate evaluates a test image against the Neural Network and returns
+%     what the network perceives the image to represent.
+%Inputs: x is an image for the network to guess on. For the purposes of this project, x is a 784x1
+%     matrix with all element values between 0 and 1. This matrix represents a 28x28 pixel image of 
+%     a handwritten digit. W1, W2, and W3 are the trained weights matrices of the Neural Network.
+%Outputs: guess is a number 0-9; it is the number the Neural Network perceives the handwritten digit to
+%     be of. y_ is the final output layer of the nerual network. It is a 10x1 matrix, with the largest 
+%     element value representing the digit the neural network perceives the image to be of. a2, a3, z2,
+%     z3, and z4 are helper structures in function evaluate. They are matrices that show the activations of
+%     each neural layer before and after compression by the sigmoid function. 
 function [guess, y_, a2, a3, z2, z3, z4] = evaluate(X, W1, W2, W3)
 z2 = feedforward(X, W1);
 a2 = arrayfun(@(x) sigmoid(x), z2);
