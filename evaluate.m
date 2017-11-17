3511117bc455b5751efc17d5e45b4f6260784fd4
thesalmonification <33437091+thesalmonification@users.noreply.github.com>
2017-11-17 09:09:51 -0600

    Update evaluate.m

--- evaluate.m
+++ evaluate.m
@@ -10,7 +10,7 @@
 %     each neural layer before and after compression by the sigmoid function. 
 function [guess, y_, a2, a3, z2, z3, z4] = evaluate(X, W1, W2, W3)
 z2 = feedforward(X, W1);
-a2 = arrayfun(@(x) sigmoid(x), z2);
+a2 = arrayfun(@(x) sigmoid(x), z2); %arrayfun allows a function to be applied to every element of a matrix
 z3 = feedforward(a2, W2);
 a3 = arrayfun(@(x) sigmoid(x), z3);
 z4 = feedforward(a3, W3);
