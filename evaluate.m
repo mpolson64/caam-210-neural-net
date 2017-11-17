fbb1eca7a900c877cedc6280eb183c4372aacc51
thesalmonification <33437091+thesalmonification@users.noreply.github.com>
2017-11-17 09:08:12 -0600

    Update evaluate.m

--- evaluate.m
+++ evaluate.m
@@ -1,6 +1,6 @@
 function [guess, y_, a2, a3, z2, z3, z4] = evaluate(X, W1, W2, W3)
 z2 = feedforward(X, W1);
-a2 = arrayfun(@(x) sigmoid(x), z2);
+a2 = arrayfun(@(x) sigmoid(x), z2); %arrayfun allows a function to be applied to every element of a matrix
 z3 = feedforward(a2, W2);
 a3 = arrayfun(@(x) sigmoid(x), z3);
 z4 = feedforward(a3, W3);
