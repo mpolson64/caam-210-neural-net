90631d1cd014f1d3b6272bde8fb3ebbd9eb22328
thesalmonification <33437091+thesalmonification@users.noreply.github.com>
2017-11-17 00:47:55 -0600

    Update scoredsgd.m

--- scoredsgd.m
+++ scoredsgd.m
@@ -1,3 +1,10 @@
+%Function scoregsd combines the trainominibatch and score functions in one, to measure improvement of the Neural Network under
+%     stochastic gradient descent.
+%Inputs: Xs_train and ys_train are inputs of the image data for the Network to train upon. W1, W2, and W3 are the weights matrices
+%     eta is the learning rate. Xs_test is test images for the Neural Network to be tested against. labels_test are the actual digit
+%     values of the test images.
+%Outputs: W1, W2, and W3 are the trained weights matrices of the system. scores is a number representing the improvement of the Network
+%     upon training. 
 function [W1, W2, W3, scores] = scoredsgd(Xs_train, ys_train, batchsize, W1, W2, W3, eta, Xs_test, labels_test, samplesize, res)
 scores = zeros(1, floor(batchsize / res));
 
