041a6b5e2dd7c41e07a3abad016ada3702fd5d76
thesalmonification <33437091+thesalmonification@users.noreply.github.com>
2017-11-16 18:17:34 -0600

    Update sigmoidprime.m

--- sigmoidprime.m
+++ sigmoidprime.m
@@ -1,3 +1,7 @@
+%Function sigmoidprime computes the value of the derivative of the sigmoid function
+%     for a given input value.
+%Input: x is a numerical value.
+%Output: y is the value of the derivative of the sigmoid function based on x. 
 function y = sigmoidprime(x)
 y = exp(-x) / ((1 + exp(-x)) ^ 2);
 end
